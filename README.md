d2518540cdacefc8ad45afa9035621cb4ab9f26f
veena <veena.g.k@ibm.com>
2023-02-08 22:08:52 +0530

    "Angular Routing"

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# project 
-hii
+# project
---
+++ angular-project/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ShopComponent } from './shop/shop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FeaturesComponent,
+    PricingComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
